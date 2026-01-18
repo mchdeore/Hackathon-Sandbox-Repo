a49d7ee81d57af5a19c1a6591c99ebc22899f02f
mchedore <marc.chedore@gmail.com>
2026-01-18 13:28:38 -0500

    MC added main game files

---
+++ MC-Subfolder/mc_sandbox.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("c");
+const ctx = canvas.getContext("2d");
+
+ctx.font = "24px sans-serif";
+ctx.fillText("Game running!", 40, 60);
--- hello.py
+++
@@ -1,4 +0,0 @@
-def mult_2(x,y): #defines mult
-    return x * y
-
-print(mult_2(3,4))  # Output: 12
